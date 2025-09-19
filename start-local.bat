258c75092478e498afa4012dbedaedef1b7daab4
moalamir52 <mo.alamir52@gmail.com>
2025-09-19 19:00:54 +0400

    password

--- start-local.bat
+++ start-local.bat
@@ -1,11 +1,4 @@
 @echo off
-echo Starting the application on local network...
-echo.
-echo The app will be available at:
-echo - Local: http://localhost:3000
-echo - Network: http://[YOUR_IP]:3000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-npm run dev
-pause
+title Starting Local Server for %CD%
+echo Starting the development server and opening in browser...
+call npm start
